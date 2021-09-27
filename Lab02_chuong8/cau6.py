f063dbee9a5ee95a8c5cf04e19b97d2daa3b81a7
huynhsang2203 <90693259+huynhsang2203@users.noreply.github.com>
2021-09-28 03:45:24 +0700

    Update 28/09

--- Lab02_chuong8/cau6.py
+++ Lab02_chuong8/cau6.py
@@ -1,14 +1,14 @@
-#Cau a. 
+#cau A. 
 temps = [25.2, 16.8, 31.4, 23.9, 28, 22.5, 19.6]
 print(temps)
-#Cau b. 
+#cau B. 
 temps.sort()
 print(temps)
-#Cau c.
+#cau C.
 cool_temps = temps[0:2]
 print(cool_temps)
 warm_temps = temps[2:]
 print(warm_temps)
-#Cau d. 
+#cau D. 
 temps_in_celsius = cool_temps + warm_temps
 print(temps_in_celsius)
