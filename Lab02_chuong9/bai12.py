89bc8593184f024378375bf0e6263b443b1a9edf
huynhsang2203 <90693259+huynhsang2203@users.noreply.github.com>
2021-09-28 18:03:54 +0700

    Update New

--- Lab02_chuong9/bai12.py
+++ Lab02_chuong9/bai12.py
@@ -1,10 +1,9 @@
+num_list=[1, 2, 3, -3, 6, -1, -3, 1]
 def remove_neg(num_list):
   index = 0
   while index < len(num_list):
-      if num_list[index] < 0:
-        del num_list[index]
-  else:
-    index += 1
-  return num_list
-remove_neg(num_list=[1, 2, 3, -3, 6, -1, -3, 1])
-
+    if num_list[index] < 0:
+      del num_list[index]
+    else:
+      index += 1
+print(remove_neg(num_list))
