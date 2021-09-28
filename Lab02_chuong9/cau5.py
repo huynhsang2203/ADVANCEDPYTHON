a144eb96337218fd1874e8082ec785c1ac2dd35f
huynhsang2203 <90693259+huynhsang2203@users.noreply.github.com>
2021-09-28 17:36:11 +0700

    Update new

--- Lab02_chuong9/cau5.py
+++ Lab02_chuong9/cau5.py
@@ -1,8 +1,10 @@
-result = [[1, 2, 3], [4, 5, 6]]
+values = [[1, 2, 3], [4, 5, 6]]
 def mystery_function(values):
+    result = []
     for sublist in values:
         result.append([sublist[0]])
-    for i in sublist[1:]:
-        result[-1].insert(0, i)
-        return result
-mystery_function(result)
+        for i in sublist[1:]:
+            result[-1].insert(0, i)
+    return result
+
+print(mystery_function(values))
