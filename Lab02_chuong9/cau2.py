aa88e28367a50f2ce3236f0871e3883583666b6b
huynhsang2203 <90693259+huynhsang2203@users.noreply.github.com>
2021-10-04 20:53:00 +0700

    Old

--- Lab02_chuong9/cau2.py
+++ Lab02_chuong9/cau2.py
@@ -1,4 +1,4 @@
 
 half_lives = [87.74, 24110.0,6537.0, 14.4, 376000.0]
 for value in half_lives:
- print(value, end=' ')
+ print(value, end='  ')
--- Lab02_chuong9/cau3.py
+++ Lab02_chuong9/cau3.py
@@ -1,5 +1,10 @@
 whales = [5, 4, 7, 3, 2, 3, 2, 6, 4, 2, 1, 7, 1, 3]
 more_whales = []
-for count in whales:
- more_whales.append(count + 1)
+for item in whales:
+ more_whales.append(item + 1)
 print(more_whales)
+#cach 2
+whales = [5, 4, 7, 3, 2, 3, 2, 6, 4, 2, 1, 7, 1, 3]
+for index, value in enumerate(whales):
+ whales[index] = value+1
+print(whales)
