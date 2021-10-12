fb549075d19a460079921bbf995e9c880c18170c
huynhsang2203 <90693259+huynhsang2203@users.noreply.github.com>
2021-10-12 19:44:05 +0700

    new

--- Lab03_chuong10/cau1.py
+++ Lab03_chuong10/cau1.py
@@ -2,6 +2,13 @@ filename = input('Which file would you like to back-up? ')
 new_filename = filename + '.bak'
 backup = open(new_filename, 'w')
 
-for line in open(filename):
-    backup.write(line)
-backup.close()
+try:
+    file = open(filename)
+    for line in file:
+        backup.write(line)
+    backup.close()
+    file.close()
+    print("Closed?",file.closed)
+except IOError:
+    print('File not found')
+print('Done')
