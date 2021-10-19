0d9012672703f4fd0e1c7cb87d4f2574ec4c963f
huynhsang2203 <90693259+huynhsang2203@users.noreply.github.com>
2021-10-19 12:47:32 +0700

    Update cau3.py

--- Lab05_chuong14/cau3.py
+++ Lab05_chuong14/cau3.py
@@ -1,21 +1,37 @@
+from typing import ClassVar
+
+
 class Student:
-    def __init__(self, fname, lname, mail, number):
+    def __init__(self, fname, lname, mail, phone):
         self.fname = fname
         self.lname = lname
         self.mail = mail
-        self.number = number
-    def __str__(self) -> str:
-        return '{} {}\n{}\n{}'.format(self.fname, self.lname, self.mail, self.number)
-    def __init__(self1, k1, k2, k3, k4, k5):
-        self1.k1 = k1
-        self1.k2 = k2
-        self1.k3 = k3 
-        self1.k4 = k4
-        self1.k5 = k5
-    def __repr__(self1) -> str:
-        return "KH('{0}','{1}','{2}','{3}','{4}')".format(self1.k1, self1.k2, self1.k3, self1.k4, self1.k5)
+        self.phone = phone 
+    def __repr__(self) -> str:
+        return 'Full Name: {} {} \nMail: {} \nPhone: {}'.format(self.fname, self.lname, self.mail, self.phone)
     pass
-student = Student('Paul','Ajax', 'pgries@cs.toronto.edu', '1234')
-khoahoc = Student('Anh', 'Toan', 'Li', 'Hoa', 'Van', 'Lag')
-print(student.__str__())
-print(khoahoc.__repr__())
+student = Student('Huynh','Sang','187it21136@vanlanguni.vn','0906256058')
+print(student.__repr__())
+print('********************************BBBBBBBBBBBBB****************************')
+class Faculty:
+  def __init__(self, industry: str, ID_industry: str, mail: str ):
+    self.industry = industry
+    self.ID_industry = ID_industry
+    self.mail = mail
+  def __repr__(self):
+    return('Chuyên nghành: {} \nKhoa: {} \nMail: {}'.format(self.industry, self.ID_industry, self.mail))
+classFaculty = Faculty('Công nghệ phần mềm', 'Công nghệ thông tin', '187IT21136@vanlanguni.vn')
+print(classFaculty.__repr__())
+print('*******************************CCCCCC********************************')
+class Member:
+    def __init__(self, name, listMember) -> None:
+        self.name = name 
+        self.listMember = listMember
+    def __repr__(self) -> str:
+        return 'Name: {} \nDS Member: {}'.format(self.name, self.listMember)
+    pass
+listMember = ['Ronaldo', 'Kaka', 'Rooney']
+
+member = Member('Python Nang Cao', listMember)
+print(member.listMember)
+print(member.__repr__())
